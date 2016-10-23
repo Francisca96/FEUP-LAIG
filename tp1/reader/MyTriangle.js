a5ce27b2387635e9ef04834ccc045ae17858fde8
Francisca96 <franciscapauperio@gmail.com>
2016-10-23 22:51:30 +0100

    Fix table .xml

--- tp1/reader/MyTriangle.js
+++ tp1/reader/MyTriangle.js
@@ -3,19 +3,19 @@
  * @param gl {WebGLRenderingContext}
  * @constructor
  */
-function MyTriangle(scene, id, x0, y0, z0, x1, y1, z1, x2, y2, z2) {
+function MyTriangle(scene, id, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
 	CGFobject.call(this,scene);
 
 	this.id=id;
-	this.x0=x0;
 	this.x1=x1;
 	this.x2=x2;
-	this.y0=y0;
+	this.x3=x3;
 	this.y1=y1;
 	this.y2=y2;
-	this.z0=z0;
+	this.y3=y3;
 	this.z1=z1;
 	this.z2=z2;
+	this.z3=z3;
 
 
 	this.initBuffers();
@@ -26,22 +26,20 @@ MyTriangle.prototype.constructor=MyTriangle;
 
 MyTriangle.prototype.initBuffers = function () {
 	this.vertices = [
-            this.x0, this.y0, this.z0,
-			this.x1, this.y1, this.z1,
-			this.x2, this.y2, this.z2
+            this.x1, this.y1, this.z1,
+			this.x2, this.y2, this.z2,
+			this.x3, this.y3, this.z3
 	];
 
 	this.indices = [
-            0, 1, 2
+            0, 1, 2,
     ];
 
- //    this.normals = [
- //            //calcular depois
- //    ];
-
-	// this.texCoords = [
-	// 	//TODO: depois ve-se
-	// ]
+    this.normals = [
+    		0, 0, 1,
+    		0, 0, 1,
+    		0, 0, 1,
+    ];
 
 	this.primitiveType=this.scene.gl.TRIANGLES;
 	this.initGLBuffers();
